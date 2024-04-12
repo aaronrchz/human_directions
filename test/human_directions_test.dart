5b47147e01b0705aed8cb5dc2eda9f3aefa83ebe
Aarón Chz <139084941+aaronrchz@users.noreply.github.com>
2024-04-12 12:45:55 -0600

    Changed value for a test

--- test/human_directions_test.dart
+++ test/human_directions_test.dart
@@ -6,17 +6,16 @@ import 'package:human_directions/components/distance_matrix.dart';
 
 void main() async {
   test('test Distance Matrix', () async {
-    final distanceMatrix =
-        await getDistanceMatrix('AIzaSyAiCuvv9hmJM1EmMOKPDWiHKwaEmSldAms',
-            origins: [
-              '35.088292431841694, -106.61910760086647',
-              '35.12869686156047, -106.61634682382963',
-            ],
-            destinations: [
-              '4200 Lomas Blvd NE, Albuquerque, NM 87110',
-              '35.0831033940459, -106.61611343660347'
-            ],
-            mode: TravelMode.walking.toString());
+    final distanceMatrix = await getDistanceMatrix('TEST_API_KEY',
+        origins: [
+          '35.088292431841694, -106.61910760086647',
+          '35.12869686156047, -106.61634682382963',
+        ],
+        destinations: [
+          '4200 Lomas Blvd NE, Albuquerque, NM 87110',
+          '35.0831033940459, -106.61611343660347'
+        ],
+        mode: TravelMode.walking.toString());
     print(distanceMatrix);
     expect(distanceMatrix, isA<List<OriginDestinationMetrics>>());
   });
